bd11d49be54082cf56af6974b5ee04814def1f5d
ccbrooks <chrisclarkbrooks@gmail.com>
2016-10-30 16:49:50 -0700

    Updates

---
+++ assets/javascript/index.js
@@ -0,0 +1,18 @@
+'use strict';
+
+console.log('Hello world!');
+'use strict';
+
+$('a[href*="#"]:not([href="#"])').click(function () {
+  if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+    var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+    if (target.length) {
+      $('html, body').animate({
+        scrollTop: target.offset().top - 120
+      }, 1000);
+      return false;
+    }
+  }
+});
+//# sourceMappingURL=index.js.map
